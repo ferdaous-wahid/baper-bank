e5b484508ab32be90662f5a30703f627479c1c2c
wahidvai <ferdaouswahid2001@gmail.com>
2022-08-17 21:46:23 +0600

    all done

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,3 +1,27 @@
-document.getElementById('btn-withdraw').addEventListener('click', function (){
-  console.log('withdraw button clicked')
-})
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+  const withdrawField = document.getElementById('withdraw-field');
+  const newWithDrawAmountString = withdrawField.value;
+  const newWithDrawAmount = parseFloat(newWithDrawAmountString);
+
+  const withdrawTotalElement = document.getElementById('withdraw-total');
+  const previousWithdrawTotalString = withdrawTotalElement.innerText;
+  const previousWithDrawTotal = parseFloat(previousWithdrawTotalString);
+
+  const balanceTotalElement = document.getElementById('balance-total');
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  if (newWithDrawAmount > previousBalanceTotal) {
+    alert('Bap er bank a ato taka nai');
+    return;
+  }
+
+  const currentWithdrawTotal = previousWithDrawTotal + newWithDrawAmount;
+  withdrawTotalElement.innerText = currentWithdrawTotal;
+
+  const newBalanceTotal = previousBalanceTotal - newWithDrawAmount;
+  balanceTotalElement.innerText = newBalanceTotal;
+
+  withdrawField.value = '';
+
+});
