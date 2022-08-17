d6bbd07f19a0b5a871bd318015772429c3317707
wahidvai <ferdaouswahid2001@gmail.com>
2022-08-17 21:30:43 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,16 @@
+document.getElementById('btn-submit').addEventListener('click', function () {
+  const emailField = document.getElementById('user-email');
+  const email = emailField.value;
+
+  // console.log(email);
+  const passwordField = document.getElementById('user-password');
+  const password = passwordField.value;
+
+  // console.log(password);
+  if (email === 'sontan@baap.com' && password === 'secret') {
+    window.location.href = "bank.html";
+  }
+  else {
+    alert('invalid user');
+  }
+});
